09325b6a37011e36911b5407ae3ad2a22c8a30f4
shamotskyi <hamotskyi@exb.de>
2021-04-22 18:13:36 +0200

    Fix typo in URI in make_request.py

--- deploy/rest-api/make_request.py
+++ deploy/rest-api/make_request.py
@@ -2,5 +2,5 @@ import requests
 
 url = 'http://localhost:3000/predict'
 
-r = requests.post(url,json={'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/2/27/Finnish_Spitz_600.jpg})
+r = requests.post(url,json={'imageUrl': 'https://upload.wikimedia.org/wikipedia/commons/2/27/Finnish_Spitz_600.jpg'})
 print(r.json())
